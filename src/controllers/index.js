ba77976d6607073999c81de19cfbfabb484e3aeb
Abadaike Collins <abadaikecollins@gmail.com>
2021-01-29 17:49:49 +0100

    updated index controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,6 +3,8 @@
  * @file App Index Controller
  */
 
+const { type } = require("os");
+
 let base;
 let currencies;
 
@@ -13,7 +15,39 @@ class Index {
 	 */
 	static async getKeys(req, res) {
 		base = req.query.base;
-		currencies = req.query.currencies;
+		currencies = req.query.currency;
+
+		return new Promise((resolve, reject) => {
+            let currencyType = Index.getCurrencyType(currencies);
+            console.log(currencyType);
+
+        }).catch((err) => {
+			console.log(err);
+			return res.status(401).json(await Index.response('Server error try later', url));
+		});
+
+	}
+
+	/**
+	 * @description Get result
+	 * @return Object {base, date, rates}
+	 */
+	static async getCurrencyType(req, res) {
+		try {
+			if (typeof currencies === 'string' || currencies instanceof String) return {type: String};
+			if (Array.isArray(currencies)) return {type: Object};
+		} catch (error) {
+			console.log('error', error);
+			return false;
+		}
+    }
+
+    /**
+	 * @description Method that respond if error
+	 * @returns Object
+	 */
+	static async response(msg, url, api = 'Currency-Rate-API') {
+		return { api, url, msg };
 	}
 }
 
